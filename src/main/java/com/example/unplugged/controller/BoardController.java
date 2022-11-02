91bb7b379493bce4f3799d1f5d68208cf609da6f
devlukej <104028946+devlukej@users.noreply.github.com>
2022-11-03 01:28:53 +0900

    ver 1.3 2022-11-03 laptop

--- src/main/java/com/example/unplugged/controller/BoardController.java
+++ src/main/java/com/example/unplugged/controller/BoardController.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @AllArgsConstructor
--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -47,149 +47,149 @@ function signUpCheck() {
     let gender_woman = document.getElementById("gender_woman").checked
     let check = true;
 
+    if (name === "" || id ==="" || pw === "" || checkpw ==="" || phone1 === "" || phone2 === "" || phone3 === "" ||
+        birthday === "" || studentNum  === "" || major === "" || year === "" || session === "" || !position_manager && !position_user
+        || !gender_man && !gender_woman) {
+        // 이름 확인
+        if (name === "") {
+            document.getElementById("nameError").innerHTML = "이름이 올바르지 않습니다."
+            check = false
+        } else {
+            document.getElementById("nameError").innerHTML = ""
+        }
 
-    // 이름 확인
-    if (name === "") {
-        document.getElementById("nameError").innerHTML = "이름이 올바르지 않습니다."
-        check = false
-    } else {
-        document.getElementById("nameError").innerHTML = ""
-    }
-
-    // 아이디 확인
-    if (id === "") {
-        document.getElementById("idError").innerHTML = "아이디가 올바르지 않습니다."
-        check = false
-    } else {
-        document.getElementById("idError").innerHTML = ""
-    }
-
-
-    // 비밀번호 확인
-    if (pw !== checkpw) {
-        document.getElementById("pwError").innerHTML = ""
-        document.getElementById("checkpwError").style.color = "rgba(248,42,42,0.7)";
-        document.getElementById("checkpwError").innerHTML = "비밀번호가 동일하지 않습니다."
-        check = false
-    } else {
-        document.getElementById("checkpwError").style.color = "rgba(16,201,197,0.7)";
-        document.getElementById("pwError").innerHTML = ""
-        document.getElementById("checkpwError").innerHTML = "비밀번호 일치함"
-    }
-
-    if (pw === "") {
-        document.getElementById("checkpwError").style.color = "rgba(248,42,42,0.7)";
-        document.getElementById("pwError").innerHTML = "비밀번호를 입력해주세요."
-        check = false
-    } else {
-        //document.getElementById("passwordError").innerHTML=""
-    }
-    if (checkpw === "") {
-        document.getElementById("checkpwError").style.color = "rgba(248,42,42,0.7)";
-        document.getElementById("checkpwError").innerHTML = "비밀번호를 다시 입력해주세요."
-        check = false
-    } else {
-        //document.getElementById("passwordCheckError").innerHTML=""
-    }
+        // 아이디 확인
+        if (id === "") {
+            document.getElementById("idError").innerHTML = "아이디가 올바르지 않습니다."
+            check = false
+        } else {
+            document.getElementById("idError").innerHTML = ""
+        }
 
 
-    // 연락처 확인
-    if (phone1 === "" || phone1.length !== 3) {
-        document.getElementById("phoneError").innerHTML = "연락처 확인"
-        check = false
-    } else {
-        document.getElementById("phoneError").innerHTML = ""
-    }
-    if (phone2 === "" || phone2.length !== 4) {
-        document.getElementById("phoneError").innerHTML = "연락처 확인"
-        check = false
-    } else {
-        document.getElementById("phoneError").innerHTML = ""
-    }
-    if (phone3 === "" || phone3.length !== 4) {
-        document.getElementById("phoneError").innerHTML = "연락처 확인"
-        check = false
-    } else {
-        document.getElementById("phoneError").innerHTML = ""
-    }
+        // 비밀번호 확인
+        if (pw !== checkpw) {
+            document.getElementById("pwError").innerHTML = ""
+            document.getElementById("checkpwError").style.color = "rgba(248,42,42,0.7)";
+            document.getElementById("checkpwError").innerHTML = "비밀번호가 동일하지 않습니다."
+            check = false
+        } else {
+            document.getElementById("checkpwError").style.color = "rgba(16,201,197,0.7)";
+            document.getElementById("pwError").innerHTML = ""
+            document.getElementById("checkpwError").innerHTML = "비밀번호 일치함"
+        }
 
-    // 학번입력 확인
-    if (studentNum === "" || studentNum.length !== 8) {
-        document.getElementById("studentNumError").innerHTML = "학번 입력"
-        check = false
-    } else {
-        document.getElementById("studentNumError").innerHTML = ""
-    }
-
-
-    // 학과선택 확인
-    if (major === "") {
-        document.getElementById("majorError").innerHTML = "학과 선택"
-        check = false
-    } else {
-        document.getElementById("majorError").innerHTML = ""
-    }
-
-    // 기수선택 확인
-    if (year === "") {
-        document.getElementById("yearError").innerHTML = "기수 선택"
-        check = false
-    } else {
-        document.getElementById("yearError").innerHTML = ""
-    }
-
-    // 세션선택 확인
-    if (session === "") {
-        document.getElementById("sessionError").innerHTML = "세션 선택"
-        check = false
-    } else {
-        document.getElementById("sessionError").innerHTML = ""
-    }
-
-    // 생년월일 확인
-    if (birthday === "") {
-        document.getElementById("birthdayError").innerHTML = "생년월일 확인"
-        check = false
-    } else {
-        document.getElementById("birthdayError").innerHTML = ""
-    }
-
-    // 직급확인
-    if (!position_manager && !position_user) {
-        document.getElementById("positionError").innerHTML = "직급 선택"
-        check = false
-    } else {
-        document.getElementById("positionError").innerHTML = ""
-    }
+        if (pw === "") {
+            document.getElementById("checkpwError").style.color = "rgba(248,42,42,0.7)";
+            document.getElementById("pwError").innerHTML = "비밀번호를 입력해주세요."
+            check = false
+        } else {
+            //document.getElementById("passwordError").innerHTML=""
+        }
+        if (checkpw === "") {
+            document.getElementById("checkpwError").style.color = "rgba(248,42,42,0.7)";
+            document.getElementById("checkpwError").innerHTML = "비밀번호를 다시 입력해주세요."
+            check = false
+        } else {
+            //document.getElementById("passwordCheckError").innerHTML=""
+        }
 
 
-    // 성별체크확인
-    if (!gender_man && !gender_woman) {
-        document.getElementById("genderError").innerHTML = "성별 선택"
-        check = false
-    } else {
-        document.getElementById("genderError").innerHTML = ""
-    }
+        // 연락처 확인
+        if (phone1 === "" || phone1.length !== 3) {
+            document.getElementById("phoneError").innerHTML = "연락처 확인"
+            check = false
+        } else {
+            document.getElementById("phoneError").innerHTML = ""
+        }
+        if (phone2 === "" || phone2.length !== 4) {
+            document.getElementById("phoneError").innerHTML = "연락처 확인"
+            check = false
+        } else {
+            document.getElementById("phoneError").innerHTML = ""
+        }
+        if (phone3 === "" || phone3.length !== 4) {
+            document.getElementById("phoneError").innerHTML = "연락처 확인"
+            check = false
+        } else {
+            document.getElementById("phoneError").innerHTML = ""
+        }
 
-    if (check) {
-        document.getElementById("nameError").innerHTML = ""
-        document.getElementById("idError").innerHTML = ""
-        document.getElementById("pwError").innerHTML = ""
-        document.getElementById("checkpwError").innerHTML = ""
-        document.getElementById("phoneError").innerHTML = ""
-        document.getElementById("studentNumError").innerHTML = ""
-        document.getElementById("majorError").innerHTML = ""
-        document.getElementById("yearError").innerHTML = ""
-        document.getElementById("sessionError").innerHTML = ""
-        document.getElementById("positionError").innerHTML = ""
-        document.getElementById("genderError").innerHTML = ""
-        document.getElementById("birthdayError").innerHTML = ""
+        // 학번입력 확인
+        if (studentNum === "" || studentNum.length !== 8) {
+            document.getElementById("studentNumError").innerHTML = "학번 입력"
+            check = false
+        } else {
+            document.getElementById("studentNumError").innerHTML = ""
+        }
 
-        //비동기 처리이벤트
-        setTimeout(function () {
-            alert("가입이 완료되었습니다.")
-        }, 0);
-    }
+
+        // 학과선택 확인
+        if (major === "") {
+            document.getElementById("majorError").innerHTML = "학과 선택"
+            check = false
+        } else {
+            document.getElementById("majorError").innerHTML = ""
+        }
+
+        // 기수선택 확인
+        if (year === "") {
+            document.getElementById("yearError").innerHTML = "기수 선택"
+            check = false
+        } else {
+            document.getElementById("yearError").innerHTML = ""
+        }
+
+        // 세션선택 확인
+        if (session === "") {
+            document.getElementById("sessionError").innerHTML = "세션 선택"
+            check = false
+        } else {
+            document.getElementById("sessionError").innerHTML = ""
+        }
+
+        // 생년월일 확인
+        if (birthday === "") {
+            document.getElementById("birthdayError").innerHTML = "생년월일 확인"
+            check = false
+        } else {
+            document.getElementById("birthdayError").innerHTML = ""
+        }
+
+        // 직급확인
+        if (!position_manager && !position_user) {
+            document.getElementById("positionError").innerHTML = "직급 선택"
+            check = false
+        } else {
+            document.getElementById("positionError").innerHTML = ""
+        }
+
+
+        // 성별체크확인
+        if (!gender_man && !gender_woman) {
+            document.getElementById("genderError").innerHTML = "성별 선택"
+            check = false
+        } else {
+            document.getElementById("genderError").innerHTML = ""
+        }
+
+        if (check) {
+            document.getElementById("nameError").innerHTML = ""
+            document.getElementById("idError").innerHTML = ""
+            document.getElementById("pwError").innerHTML = ""
+            document.getElementById("checkpwError").innerHTML = ""
+            document.getElementById("phoneError").innerHTML = ""
+            document.getElementById("studentNumError").innerHTML = ""
+            document.getElementById("majorError").innerHTML = ""
+            document.getElementById("yearError").innerHTML = ""
+            document.getElementById("sessionError").innerHTML = ""
+            document.getElementById("positionError").innerHTML = ""
+            document.getElementById("genderError").innerHTML = ""
+            document.getElementById("birthdayError").innerHTML = ""
+
+            }
+        } else {
+    };
 }
 
 function getMajor()  {
