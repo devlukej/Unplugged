10441549e35eda706767c3450f699d636694afee
devlukej <dev.lukej@gmail.com>
2022-11-21 22:26:54 +0900

    ver 1.5 2022-11-17 PC

--- src/test/java/com/example/unplugged/UnpluggedApplicationTests.java
+++ src/test/java/com/example/unplugged/UnpluggedApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.unplugged;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class UnpluggedApplicationTests {
 
 	@Test
